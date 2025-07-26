7bc00e2892c96b56c2d92ecb796d6d725ea2c96b
聆听·彼岸 <38424427+jnuse@users.noreply.github.com>
2025-07-27 00:57:50 +0800

    Update webpack.prod.conf.js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,48 +1,116 @@
-var path = require("path");
-var utils = require("./utils");
-var webpack = require("webpack");
-var config = require("../config");
-var merge = require("webpack-merge");
-var baseWebpackConfig = require("./webpack.base.conf");
-var CleanWebpackPlugin = require("clean-webpack-plugin");
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
-var env = config.build.env;
+const env = config.build.env
 
-var webpackConfig = merge(baseWebpackConfig, {
+const webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
       extract: true
     })
   },
-  devtool: config.build.productionSourceMap ? "#source-map" : false,
+  devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath("js/[name].[chunkhash].js"),
-    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js")
+    filename: utils.assetsPath('js/[name].[chunkhash].js'),
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
-  // output: {
-  //   path: config.build.assetsRoot,
-  //   filename: utils.assetsPath('js/[name].js'),
-  //   chunkFilename: utils.assetsPath('js/[id].js')
-  // },
-  plugins: []
-});
+  plugins: [
+    // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: true
+    }),
+    // 💡 关键！这就是那个缺失的“口袋”！
+    new ExtractTextPlugin({
+      filename: utils.assetsPath('css/[name].[contenthash].css')
+    }),
+    // 压缩提取出的CSS，并处理来自不同组件的重复CSS
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        safe: true
+      }
+    }),
+    // 💡 关键！这就是那个负责“装盘”的插件，会生成最终的 index.html
+    new HtmlWebpackPlugin({
+      filename: config.build.index,
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+      chunksSortMode: 'dependency'
+    }),
+    // 保持 module.id 的稳定
+    new webpack.HashedModuleIdsPlugin(),
+    // 将第三方库 (vendor) 分离到自己的文件
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        )
+      }
+    }),
+    // 提取 webpack 运行环境和 manifest
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor']
+    }),
+    // 复制静态资源
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static'),
+        to: config.build.assetsSubDirectory,
+        ignore: ['.*']
+      }
+    ])
+  ]
+})
 
 if (config.build.productionGzip) {
-  var CompressionWebpackPlugin = require("compression-webpack-plugin");
+  const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
       test: new RegExp(
-        "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
+        '\\.(' +
+        config.build.productionGzipExtensions.join('|') +
+        ')$'
       ),
       threshold: 10240,
       minRatio: 0.8
     })
-  );
+  )
 }
 
-module.exports = webpackConfig;
+if (config.build.bundleAnalyzerReport) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+}
+
+module.exports = webpackConfig
